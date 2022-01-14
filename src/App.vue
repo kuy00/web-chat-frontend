dde165d54d60f578c6bab14ac885ca108eaed62a
kuy <kuy472013@gmail.com>
2022-01-14 23:51:53 +0900

    코드 컨벤션 수정

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,19 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width : 100vw;
+  height : 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- src/components/Socket.vue
+++ src/components/Socket.vue
@@ -1,33 +1,33 @@
 <template>
-    <div>
-        <button v-on:click="connect">connect</button>
-        <textarea v-model="message" />
-    </div>
+  <div>
+    <button v-on:click="connect">connect</button>
+    <textarea v-model="message" />
+  </div>
 </template>
 
 <script>
 import Echo from "laravel-echo"
 
 export default {
-    name: 'Socket',
-    data() {
-        return {
-            socket: null,
-            message: '',
-        }
+  name: "Socket",
+  data: function () {
+    return {
+      socket: null,
+      message: "",
+    }
+  },
+  methods: {
+    connect: function () {
+      window.io = require("socket.io-client")
+      this.socket = new Echo({
+        broadcaster: "socket.io",
+        host: "http://localhost:6001",
+      })
+      this.socket.channel("laravel_database_test")
+      .listen("test", (e) => {
+        this.message = e.message
+      })
     },
-    methods: {
-        connect: function () {
-            window.io = require('socket.io-client')
-            this.socket = new Echo({
-                broadcaster: 'socket.io',
-                host: 'http://localhost:6001',
-            })
-            this.socket.channel('laravel_database_test')
-                .listen('test', (e) => {
-                    this.message = e.message
-                })
-        },
-    },
+  },
 }
 </script>
