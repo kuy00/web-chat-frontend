f0292a6f7cfeeef6dc0f7e21db4fcb08eaeb628c
kuy <kuy472013@gmail.com>
2022-03-06 12:49:44 +0900

    패키지 설정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <notifications position="bottom center" />
+    <Notification />
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
 import store from './store'
-import Notifications from '@kyvg/vue3-notification'
+import Notifications from '@kuy472013/vue-notification'
+import SvgLoader from 'svg-loader-vue'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(SvgLoader)
 app.use(Notifications)
 app.mount('#app')
