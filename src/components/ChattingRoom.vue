c338bf982999e7f8b9affb8441ce4a79ddc77845
kuy <kuy472013@gmail.com>
2022-03-13 23:17:33 +0900

    채팅방 디자인

--- src/components/ChattingRoom.vue
+++ src/components/ChattingRoom.vue
@@ -1,17 +1,77 @@
 <template>
   <div id='chatting_room'>
+    <div id='top-container' v-if='isVisible'>
+      <svg-loader
+        id='svg-container'
+        :svgFile='leftSvg'
+        :width=30
+        :height=30
+        fill='gray'
+        @click='prev'
+      />
+      <div id='image-container'>
+        <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F2207573D58CFDE2704BB29">
+      </div>
+      <div>
+        <span>{{modelValue.name}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import left from '@/assets/left.svg'
+
 export default {
   name: 'ChattingRoom',
+  props: {
+    modelValue: Object,
+  },
+  data: function () {
+    return {
+      leftSvg: left,
+    }
+  },
+  computed: {
+    isVisible: function () {
+      return !(Object.keys(this.modelValue).length === 0)
+    },
+  },
+  methods: {
+    prev: function () {
+      this.$emit('update:modelValue', {})
+    },
+  },
 }
 </script>
 
 <style scoped>
   #chatting_room {
     flex: 2;
-    background-color: aquamarine;
+    background-color: #f2f3f5;
+  }
+  #top-container {
+    height: 70px;
+    background-color: #ebeced;
+    display: flex;
+  }
+  #top-container div {
+    align-self: center;
+  }
+  #svg-container {
+    margin: 0px 15px;
+    cursor: pointer;
+  }
+  #image-container {
+    width: 60px;
+    height: 60px;
+    border-radius: 20px;
+    overflow: hidden;
+    margin-right: 10px;
+  }
+  #image-container img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 </style>
