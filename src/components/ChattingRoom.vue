bd9d89b0c81df6964a7d389233d3ff1437ede655
kuy <kuy472013@gmail.com>
2022-03-17 22:58:44 +0900

    채팅방 디자인 수정

--- src/components/ChattingRoom.vue
+++ src/components/ChattingRoom.vue
@@ -1,19 +1,39 @@
 <template>
-  <div id='chatting_room'>
-    <div id='top-container' v-if='isVisible'>
-      <svg-loader
-        id='svg-container'
-        :svgFile='leftSvg'
-        :width=30
-        :height=30
-        fill='gray'
-        @click='prev'
-      />
-      <div id='image-container'>
-        <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F2207573D58CFDE2704BB29">
+  <div id='chatting-room'>
+    <div v-if='isVisible'>
+      <div id='top-container'>
+        <svg-loader
+          id='svg-container'
+          :svgFile='leftSvg'
+          :width=30
+          :height=30
+          fill='gray'
+          @click='prev'
+        />
+        <div id='image-container'>
+          <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F2207573D58CFDE2704BB29">
+        </div>
+        <div>
+          <span>{{modelValue.name}}</span>
+        </div>
       </div>
-      <div>
-        <span>{{modelValue.name}}</span>
+      <div id='main-container'>
+      </div>
+      <div id='bottom-container'>
+        <div>
+          <div id="input-container">
+            <input />
+            <div>
+              <svg-loader
+                id='svg-container'
+                :svgFile='sendSvg'
+                :width=30
+                :height=30
+                fill='white'
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,6 +41,7 @@
 
 <script>
 import left from '@/assets/left.svg'
+import send from '@/assets/send.svg'
 
 export default {
   name: 'ChattingRoom',
@@ -30,6 +51,7 @@ export default {
   data: function () {
     return {
       leftSvg: left,
+      sendSvg: send,
     }
   },
   computed: {
@@ -46,21 +68,28 @@ export default {
 </script>
 
 <style scoped>
-  #chatting_room {
+  #chatting-room {
     flex: 2;
     background-color: #f2f3f5;
   }
+  #chatting-room > div {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
   #top-container {
-    height: 70px;
+    flex: 1;
     background-color: #ebeced;
     display: flex;
+    padding: 10px 0px;
   }
   #top-container div {
     align-self: center;
   }
   #svg-container {
-    margin: 0px 15px;
+    margin: 0px 11px;
     cursor: pointer;
+    align-self: center;
   }
   #image-container {
     width: 60px;
@@ -73,5 +102,46 @@ export default {
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+  #main-container {
+    flex: 25;
+  }
+  #bottom-container {
+    flex: 1;
+    padding: 10px;
+  }
+  #bottom-container > div {
+    background-color: white;
+    min-height: 60px;
+    height: 100%;
+    border-radius: 10px;
+    display: flex;
+    border: 2px solid gray;
+  }
+  #input-container {
+    display: flex;
+    overflow: hidden;
+    width: 100%;
+    padding: 0px 10px;
+  }
+  #input-container > input {
+    width: 100%;
+    height: 100%;
+    align-self: center;
+    border: none;
+    padding: 10px;
+    font-size: 20px;
+  }
+  #input-container > input:focus {
+    outline: none;
+  }
+  #input-container > div {
+    background-color: gray;
+    width: 50px;
+    min-height: 50px;
+    height: 75%;
+    border-radius: 50px;
+    display: flex;
+    align-self: center;
   }
 </style>
